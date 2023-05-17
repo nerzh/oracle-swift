2e617454091ce22d89640ff9d363b434273b617d
nerzh <emptystamp@gmail.com>
2023-05-17 03:56:13 +0200

    fix pck

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let name: String = "oracle-swift"
 
 var packageDependencies: [Package.Dependency] = [
-    .package(path: "/Users/nerzh/mydata/swift_projects/adnl-swift")
+//    .package(path: "/Users/nerzh/mydata/swift_projects/adnl-swift")
 //    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.45.0")),
 //    .package(url: "https://github.com/nerzh/VaporBridges.git", branch: "master"),
 //    .package(url: "https://github.com/nerzh/PostgresBridge.git", branch: "master"),
@@ -19,27 +19,12 @@ var packageDependencies: [Package.Dependency] = [
 
 var mainTarget: [Target.Dependency] = [
     .product(name: "adnl-swift", package: "adnl-swift"),
-//    .product(name: "Vapor", package: "vapor"),
-//    .product(name: "PostgresBridge", package: "PostgresBridge"),
-//    .product(name: "VaporBridges", package: "VaporBridges"),
-//    .product(name: "Bridges", package: "Bridges"),
-//    .product(name: "SwiftRegularExpression", package: "swift-regular-expression"),
-//    .product(name: "EverscaleClientSwift", package: "everscale-client-swift"),
-//    .product(name: "FileUtils", package: "SwiftFileUtils"),
-//    .product(name: "SwiftExtensionsPack", package: "swift-extensions-pack"),
-//    .product(name: "IkigaJSON", package: "IkigaJSON"),
-//    .product(name: "BigInt", package: "BigInt"),
-//    .product(name: "Swiftgger", package: "Swiftgger"),
 ]
 
 #if os(Linux)
-//packageDependencies.append(.package(url: "https://github.com/nerzh/Swiftgger", branch: "master"))
-//packageDependencies.append(.package(url: "https://github.com/nerzh/swift-extensions-pack", branch: "master"))
-//packageDependencies.append(.package(url: "https://github.com/nerzh/everscale-client-swift", branch: "master"))
+packageDependencies.append(.package(url: "https://github.com/nerzh/adnl-swift", branch: "master"))
 #else
-//packageDependencies.append(.package(path: "/Users/nerzh/mydata/swift_projects/Swiftgger"))
-//packageDependencies.append(.package(path: "/Users/nerzh/mydata/swift_projects/swift-extensions-pack"))
-//packageDependencies.append(.package(path: "/Users/nerzh/mydata/swift_projects/everscale-client-swift"))
+packageDependencies.append(.package(path: "/Users/nerzh/mydata/swift_projects/adnl-swift"))
 #endif
 
 let package = Package(
